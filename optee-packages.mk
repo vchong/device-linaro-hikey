de45aa7cc1441269079a52a2399bf968d9750e50
Yongqin Liu <yongqin.liu@linaro.org>
2016-06-21 16:26:31 +0800

    optee: integrate for optee components
    Change-Id: I998e969c90356f73133248cce0a1ee647623710a
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

---
+++ optee-packages.mk
@@ -0,0 +1,24 @@
+OPTEE_PLATFORM ?= hikey
+OPTEE_CFG_ARM64_CORE ?= y
+OPTEE_TA_TARGETS ?= ta_arm64
+OPTEE_OS_DIR ?= optee/optee_os
+BUILD_OPTEE_MK := $(OPTEE_OS_DIR)/mk/aosp_optee.mk
+
+# OP TEE client library and service
+PRODUCT_PACKAGES += libteec \
+                    tee-supplicant \
+
+# optee_test and TA
+PRODUCT_PACKAGES += xtest
+PRODUCT_PACKAGES += 5b9e0e40-2636-11e1-ad9e-0002a5d5c51b.ta
+PRODUCT_PACKAGES += 5ce0c432-0ab0-40e5-a056-782ca0e6aba2.ta
+PRODUCT_PACKAGES += 614789f2-39c0-4ebf-b235-92b32ac107ed.ta
+PRODUCT_PACKAGES += 731e279e-aafb-4575-a771-38caa6f0cca6.ta
+PRODUCT_PACKAGES += b689f2a7-8adf-477a-9f99-32e90c0ad0a2.ta
+PRODUCT_PACKAGES += c3f6e2c0-3548-11e1-b86c-0800200c9a66.ta
+PRODUCT_PACKAGES += cb3e5ba0-adf1-11e0-998b-0002a5d5c51b.ta
+PRODUCT_PACKAGES += d17f73a0-36ef-11e1-984a-0002a5d5c51b.ta
+PRODUCT_PACKAGES += e13010e0-2ae1-11e5-896a-0002a5d5c51b.ta
+PRODUCT_PACKAGES += e626662e-c0e2-485c-b8c8-09fbce6edf3d.ta
+PRODUCT_PACKAGES += e6a33ed4-562b-463a-bb7e-ff5e15a493c8.ta
+PRODUCT_PACKAGES += f157cda0-550c-11e5-a6fa-0002a5d5c51b.ta
