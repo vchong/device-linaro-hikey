e67d50152c6e91c3284ab4fbf8511d1d646432c1
Yongqin Liu <yongqin.liu@linaro.org>
2017-03-10 18:18:06 +0800

    boot parameters: set printk.devkmsg to on
    by default ratelimit, message from init command would not be
    output to kernel message like the following message:
        [    7.929958] init: Starting service 'surfaceflinger'...
    
    here we set to not limit the userspace logging
    
    also update to support boot_fat.uefi.img with nougat
    
    Change-Id: Icf8353275c9f080a1ce4532029ed7ee0e49797d3
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- hikey/BoardConfig.mk
+++ hikey/BoardConfig.mk
@@ -2,9 +2,9 @@ include device/linaro/hikey/BoardConfigCommon.mk
 
 TARGET_BOARD_PLATFORM := hikey
 ifeq ($(TARGET_KERNEL_USE), 4.1)
-BOARD_KERNEL_CMDLINE := console=ttyAMA3,115200 androidboot.console=ttyAMA3 androidboot.hardware=hikey firmware_class.path=/system/etc/firmware efi=noruntime
+BOARD_KERNEL_CMDLINE := console=ttyAMA3,115200 androidboot.console=ttyAMA3 androidboot.hardware=hikey firmware_class.path=/system/etc/firmware efi=noruntime printk.devkmsg=on
 else
-BOARD_KERNEL_CMDLINE := console=ttyFIQ0 androidboot.console=ttyFIQ0 androidboot.hardware=hikey firmware_class.path=/system/etc/firmware efi=noruntime
+BOARD_KERNEL_CMDLINE := console=ttyFIQ0 androidboot.console=ttyFIQ0 androidboot.hardware=hikey firmware_class.path=/system/etc/firmware efi=noruntime printk.devkmsg=on
 endif
 
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
