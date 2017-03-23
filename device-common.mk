530368ea673790ae4d42b5d3ee6040e8627973aa
Yongqin Liu <yongqin.liu@linaro.org>
2017-03-23 15:58:03 +0800

    device-common.mk: disable bluetooth
    till we have workaround to make the implementation
    here for master work with nougat
    
    Change-Id: I9963f72c6eea984719ac9fa502c828f82c5c5875
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- device-common.mk
+++ device-common.mk
@@ -49,6 +49,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64_32
 PRODUCT_COPY_FILES += system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += config.disable_bluetooth=true
+
 PRODUCT_PACKAGES += libGLES_android
 
 PRODUCT_PACKAGES +=	TIInit_11.8.32.bts \
