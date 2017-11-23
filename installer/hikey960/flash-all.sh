4ed67c6723271ef5daf23e8e33edc0a05c3da01c
Victor Chong <victor.chong@linaro.org>
2017-11-23 14:23:41 +0000

    flash-all.sh: Cp sec_xloader.img from tools-images-hikey960
    to dlh/installer/hikey960/

--- installer/hikey960/flash-all.sh
+++ installer/hikey960/flash-all.sh
@@ -15,6 +15,9 @@ fi
 
 echo "android out dir:${ANDROID_PRODUCT_OUT}"
 
+cp ${ANDROID_BUILD_TOP}/device/linaro/tools-images-hikey960/sec_xloader.img \
+	${INSTALLER_DIR}/
+
 #fastboot flash xloader "${INSTALLER_DIR}"/hisi-sec_xloader.img
 #fastboot flash ptable "${INSTALLER_DIR}"/hisi-ptable.img
 #fastboot flash fastboot "${INSTALLER_DIR}"/hisi-fastboot.img
