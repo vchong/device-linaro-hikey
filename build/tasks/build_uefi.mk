fd0efa1b37a5a7036530b21fc126ca9bee0c1db8
Victor Chong <victor.chong@linaro.org>
2017-12-20 18:25:03 +0000

    use var for uefi GCC# n add dbg prints for some env vars

--- build/tasks/build_uefi.mk
+++ build/tasks/build_uefi.mk
@@ -6,7 +6,18 @@ BOOTLOADER_DIR ?= $(ANDROID_BUILD_TOP)/device/linaro/hikey/bootloader
 DIST_DIR_PRODUCT ?= $(ANDROID_BUILD_TOP)/out/dist
 TARGET_TEE_IS_OPTEE ?= true
 
-$(DIST_DIR_PRODUCT)/fip.bin: $(BOOTLOADER_DIR)/Makefile
+.phony: printdir
+printdir:
+	echo "ANDROID_BUILD_TOP = $(ANDROID_BUILD_TOP)"
+	echo "BOOTLOADER_DIR = $(BOOTLOADER_DIR)"
+	echo "DIST_DIR_PRODUCT = $(DIST_DIR_PRODUCT)"
+	echo "OUT_DIR = $(OUT_DIR)"
+	echo "HOST_OUT = $(HOST_OUT)"
+	echo "PRODUCT_OUT = $(PRODUCT_OUT)"
+	echo "TOP = $(TOP)"
+	ls -l $(TOP)/
+
+$(DIST_DIR_PRODUCT)/fip.bin: printdir $(BOOTLOADER_DIR)/Makefile
 	$(MAKE) -C $(BOOTLOADER_DIR) DIST_DIR=$(DIST_DIR_PRODUCT) TARGET_TEE_IS_OPTEE=$(TARGET_TEE_IS_OPTEE) all
 	@#cp $@ $(ANDROID_BUILD_TOP)/device/linaro/hikey/installer/hikey960
 
