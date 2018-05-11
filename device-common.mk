713af49e86db530ff16cc46a8e2d2f03bf3fcc0a
Victor Chong <victor.chong@linaro.org>
2018-05-11 16:22:07 +0100

    Merge -impl into -service
    Change-Id: Icbdfae5666c07a61900de128eb7d5d6bc5dce14c
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- device-common.mk
+++ device-common.mk
@@ -82,13 +82,19 @@ PRODUCT_PACKAGES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl.optee \
     android.hardware.keymaster@3.0-service.optee
+
+#PRODUCT_PACKAGES += \
+#    android.hardware.keymaster@3.0-impl.optee \
+#    android.hardware.keymaster@3.0-service.optee
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl.optee \
     android.hardware.gatekeeper@1.0-service.optee
+
+#PRODUCT_PACKAGES += \
+#    android.hardware.gatekeeper@1.0-impl.optee \
+#    android.hardware.gatekeeper@1.0-service.optee
 
 # Copy hardware config file(s)
 PRODUCT_COPY_FILES +=  \
