b964d86cf61b49d9bbc02a4210d34523cfceccb8
Yongqin Liu <yongqin.liu@linaro.org>
2019-04-10 17:50:29 +0800

    hikey960: workaround to compiling kernel for hikey960
    Change-Id: I41b87472422a56217ba87b9427d12867fa032b1a
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

---
+++ build/tasks/build_hikey960_kernel.mk
@@ -0,0 +1,8 @@
+ifneq ($(filter hikey960, $(TARGET_DEVICE)),)
+
+ifeq ($(TARGET_BUILD_KERNEL), true)
+$(PRODUCT_OUT)/hi3660-hikey960.dtb: $(PRODUCT_OUT)/obj/kernel/arch/arm64/boot/hi3660-hikey960.dtb
+	cp $< $@
+
+endif
+endif
--- hikey960.mk
+++ hikey960.mk
@@ -1,8 +1,15 @@
+ifneq ($(TARGET_BUILD_KERNEL), true)
 ifndef TARGET_KERNEL_USE
 TARGET_KERNEL_USE=5.4
 endif
 TARGET_PREBUILT_KERNEL := device/linaro/hikey-kernel/hikey960/$(TARGET_KERNEL_USE)/Image.gz-dtb
 TARGET_PREBUILT_DTB := device/linaro/hikey-kernel/hikey960/$(TARGET_KERNEL_USE)/hi3660-hikey960.dtb
+
+else # TARGET_BUILD_KERNEL == true for following
+TARGET_PREBUILT_KERNEL :=
+TARGET_PREBUILT_DTB :=
+TARGET_KERNEL_USE ?= 4.14
+endif # end of TARGET_BUILD_KERNEL
 
 ifeq ($(TARGET_KERNEL_USE), 4.4)
   HIKEY_USE_DRM_HWCOMPOSER := false
