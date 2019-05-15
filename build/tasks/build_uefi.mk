1fb1c818171b2ac18e660584fd5c2b68572c7f3e
Victor Chong <victor.chong@linaro.org>
2019-05-15 13:06:14 +0100

    bootloader/Makefile: make fip depend on optee_os..
    ..as it should
    
    Do this for hikey960 too!
    
    Change-Id: I9766c1d3f3988e91018c9c9a578acb53633db74e
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- build/tasks/build_uefi.mk
+++ build/tasks/build_uefi.mk
@@ -14,7 +14,22 @@ endif
 
 HOST_PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-$(DIST_DIR_PRODUCT)/fip.bin: $(BOOTLOADER_DIR)/Makefile
+# does NOT work - donno y
+# cos OPTEE_OS_BIN is NOT a real make tgt unlike OPTEE_BIN in
+# optee_os/mk/aosp_optee.mk?
+#OPTEE_OS_DIR=$(TOP_ROOT_ABS)/optee/optee_os
+#OPTEE_OS_BIN := $(OPTEE_OS_DIR)/out/arm-plat-hikey/core/tee.bin
+#$(OPTEE_OS_BIN) : $(sort $(shell find -L $(OPTEE_OS_DIR)))
+#$(DIST_DIR_PRODUCT)/fip.bin: $(BOOTLOADER_DIR)/Makefile $(OPTEE_OS_BIN)
+
+# this seems to work
+# BUT unlike bootloader/Makefile, do NOT append $(TOP_ROOT_ABS)
+# else get
+# make: *** /home/victor.chong/work/swg/aosp/wv//home/victor.chong/work/swg/aosp/wv/optee/optee_os: No such file or directory.  Stop.
+# from optee_os/mk/aosp_optee.mk when making all
+OPTEE_OS_DIR=optee/optee_os
+
+$(DIST_DIR_PRODUCT)/fip.bin: $(BOOTLOADER_DIR)/Makefile $(sort $(shell find -L $(OPTEE_OS_DIR)))
 	rm -fr $(DIST_DIR_PRODUCT) $(OPTEE_OS_DIR)/out $(PRODUCT_OUT)/obj/optee
 	PATH=$(HOST_PATH):$$PATH CROSS_COMPILE_32=$(CROSS_COMPILE32) CROSS_COMPILE_64=$(CROSS_COMPILE64) $(MAKE) -C $(BOOTLOADER_DIR) DIST_DIR=$(CURDIR)/$(DIST_DIR_PRODUCT) TARGET_TEE_IS_OPTEE=$(TARGET_TEE_IS_OPTEE) -j1 clean
 	PATH=$(HOST_PATH):$$PATH CROSS_COMPILE_32=$(CROSS_COMPILE32) CROSS_COMPILE_64=$(CROSS_COMPILE64) $(MAKE) -C $(BOOTLOADER_DIR) DIST_DIR=$(CURDIR)/$(DIST_DIR_PRODUCT) TARGET_TEE_IS_OPTEE=$(TARGET_TEE_IS_OPTEE) all
