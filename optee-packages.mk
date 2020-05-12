a138956af010d9af19c578bca6361283d9173279
Victor Chong <victor.chong@linaro.org>
2020-05-12 11:15:16 +0100

    android_flags.mk
    Change-Id: Id2d579e4e4aed572d5e99941255413da5f397f29

--- optee-packages.mk
+++ optee-packages.mk
@@ -3,11 +3,15 @@ OPTEE_PLATFORM_FLAVOR ?= $(TARGET_DEVICE)
 OPTEE_CFG_ARM64_CORE ?= y
 OPTEE_TA_TARGETS ?= ta_arm64
 OPTEE_OS_DIR ?= optee/optee_os
+OPTEE_EXTRA_FLAGS ?= CFG_TEE_CORE_LOG_LEVEL=3 CFG_TEE_TA_LOG_LEVEL=3
+# done in optee_os/android_flags.mk
+#OPTEE_EXTRA_FLAGS += CFG_CORE_HEAP_SIZE=196608
 BUILD_OPTEE_MK := $(OPTEE_OS_DIR)/mk/aosp_optee.mk
 
 # OP TEE client library and service
 PRODUCT_PACKAGES += libteec \
-                    tee-supplicant
+                    tee-supplicant \
+                    libckteec
 
 # optee_test and TA
 PRODUCT_PACKAGES += xtest
