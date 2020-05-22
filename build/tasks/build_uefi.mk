70afc7411913f5cf7bf22ac9fef9f767e6ac0375
Victor Chong <victor.chong@linaro.org>
2020-05-22 11:09:52 +0100

    wip
    Change-Id: I9ba25603e77403b20bb9016e37345974bfbdcad3

--- build/tasks/build_uefi.mk
+++ build/tasks/build_uefi.mk
@@ -2,55 +2,54 @@ ifneq ($(filter hikey%, $(TARGET_DEVICE)),)
 
 ifeq ($(TARGET_BUILD_UEFI), true)
 
-# setting value for PREBUILT_MAKE to specify which value to use
-PREBUILT_MAKE ?= prebuilts/build-tools/linux-x86/bin/make
-ifneq (,$(wildcard $(PREBUILT_MAKE)))
-HOST_MAKE := $(PREBUILT_MAKE)
-ifneq (,$(filter -j%, $(MAKE)))
-HOST_MAKE += $(filter -j%, $(MAKE))
-endif
-else
-HOST_MAKE := $(MAKE)
-endif
-
-BOOTLOADER_DIR ?= device/linaro/hikey/bootloader
-DIST_DIR_PRODUCT ?= $(OUT_DIR)/dist
-ABS_DIST_DIR_PRODUCT ?= $(realpath $(OUT_DIR))/dist
-
-TARGET_TEE_IS_OPTEE ?= false
-
-# CLANG is provided as a RO env var by soong
-# and CANNOT be overwritten
 TOP_ROOT_ABS := $(realpath $(TOP))
 
-# WARNING: MUST HAVE / AT THE END!
-CLANG_PATH := $(TOP_ROOT_ABS)/$(LLVM_PREBUILTS_PATH)/
+LLOADER_DIR=$(TOP_ROOT_ABS)/optee/l-loader
+TOOLS_DIR=$(TOP_ROOT_ABS)/optee/tools-images-hikey960
 
-#CROSS_COMPILERS := COMPILER=clang
-CROSS_COMPILERS += CROSS_COMPILE64=aarch64-linux-android-
-CROSS_COMPILERS += CROSS_COMPILE32=arm-linux-androideabi-
+DIST_DIR_PRODUCT?=$(TOP_ROOT_ABS)/out/dist
+INSTALLER_DIR?=$(TOP_ROOT_ABS)/device/linaro/hikey/installer/$(TARGET_OUT_DIR)
 
-HOST_PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+# CLANG is provided as a RO env var by soong
+# Note the trailing forward slash!
+CLANG_PATH=$(TOP_ROOT_ABS)/$(LLVM_PREBUILTS_PATH)/
+
+# Please use DEBUG=1 (default) in build_uefi.sh due to
+# https://bugs.96boards.org/show_bug.cgi?id=806
+
+FIP_BIN ?= $(DIST_DIR_PRODUCT)/fip.bin
 
 # Do NOT prepend $(TOP_ROOT_ABS) here! Else get error
 # make: *** /home/first.last/aosp//home/first.last/aosp/optee/optee_os: No such file or directory.  Stop.
 # from optee_os/mk/aosp_optee.mk when building.
 OPTEE_OS_DIR=optee/optee_os
 
+clean:
+	@# Should we really rm $(PRODUCT_OUT)/optee built by optee_os/aosp_optee.mk and NOT by this file?
+	rm -rf $(DIST_DIR_PRODUCT) $(PRODUCT_OUT)/optee
+	cd $(LLOADER_DIR)/../ && \
+		CLEAN=2 $(LLOADER_DIR)/build_uefi.sh
+
 # rebuild fip.bin whenever optee_os is modified
-$(DIST_DIR_PRODUCT)/fip.bin: $(BOOTLOADER_DIR)/Makefile $(sort $(shell find -L $(OPTEE_OS_DIR)))
-	rm -fr $(DIST_DIR_PRODUCT) $(OPTEE_OS_DIR)/out $(PRODUCT_OUT)/obj/optee
-	PATH=$(HOST_PATH):$$PATH $(CROSS_COMPILERS) $(HOST_MAKE) -C $(BOOTLOADER_DIR) DIST_DIR=$(ABS_DIST_DIR_PRODUCT) TARGET_TEE_IS_OPTEE=$(TARGET_TEE_IS_OPTEE) -j1 clean
-	PATH=$(HOST_PATH):$$PATH $(CROSS_COMPILERS) $(HOST_MAKE) -C $(BOOTLOADER_DIR) DIST_DIR=$(ABS_DIST_DIR_PRODUCT) TARGET_TEE_IS_OPTEE=$(TARGET_TEE_IS_OPTEE) all
-	cp $@ device/linaro/hikey/installer/$(TARGET_DEVICE)/
-
-$(DIST_DIR_PRODUCT)/l-loader.bin: $(DIST_DIR_PRODUCT)/fip.bin
-	cp $@ device/linaro/hikey/installer/$(TARGET_DEVICE)/
-
-$(DIST_DIR_PRODUCT)/prm_ptable.img: $(DIST_DIR_PRODUCT)/l-loader.bin
-	cp $@ device/linaro/hikey/installer/$(TARGET_DEVICE)/
-
-droidcore: $(DIST_DIR_PRODUCT)/fip.bin $(DIST_DIR_PRODUCT)/l-loader.bin $(DIST_DIR_PRODUCT)/prm_ptable.img
+# clean before build
+$(FIP_BIN): clean $(sort $(shell find -L $(OPTEE_OS_DIR)))
+	cd $(LLOADER_DIR)/../ && \
+		CLANG=CLANG_5_0 CLANG_PATH=$(CLANG_PATH) $(LLOADER_DIR)/build_uefi.sh && \
+		cp fip.bin $(DIST_DIR_PRODUCT)/ && \
+		cp fip.bin $(INSTALLER_DIR)/ && \
+		cp l-loader.bin $(DIST_DIR_PRODUCT)/ && \
+		cp l-loader.bin $(INSTALLER_DIR)/ && \
+		cp ptable-*.img $(DIST_DIR_PRODUCT)/ && \
+		cp ptable-*.img $(INSTALLER_DIR)/
+ifneq ($(filter hikey960%, $(TARGET_OUT_DIR)),)
+	cd $(LLOADER_DIR) && \
+		cp recovery.bin $(DIST_DIR_PRODUCT)/ && \
+		cp recovery.bin $(INSTALLER_DIR)/ && \
+		cp $(TOOLS_DIR)/hikey_idt $(INSTALLER_DIR)/ && \
+		cp $(TOOLS_DIR)/hisi*.img $(INSTALLER_DIR)/
 endif
 
-endif
+droidcore: $(FIP_BIN)
+endif #TARGET_BUILD_UEFI
+
+endif #
