54e442adbde358e70f9afdfe8d632018d8ab3bdd
Victor Chong <victor.chong@linaro.org>
2020-05-26 06:43:06 +0100

    wip
    Change-Id: I13137dae462f90693251e33c5637321438bce3a2

--- build/tasks/build_uefi.mk
+++ build/tasks/build_uefi.mk
@@ -4,10 +4,14 @@ ifeq ($(TARGET_BUILD_UEFI), true)
 
 TOP_ROOT_ABS := $(realpath $(TOP))
 
+OPTEE_DIR=$(TOP_ROOT_ABS)/optee
 LLOADER_DIR=$(TOP_ROOT_ABS)/optee/l-loader
 TOOLS_DIR=$(TOP_ROOT_ABS)/optee/tools-images-hikey960
 
-DIST_DIR_PRODUCT?=$(TOP_ROOT_ABS)/out/dist
+# https://sx.ix5.org/info/post/android-q-compiling-kernels-in-tree
+# make target MUST beging with `out/..` and NOT abs path!
+DIST_DIR_PRODUCT?=$(OUT_DIR)/dist
+
 INSTALLER_DIR?=$(TOP_ROOT_ABS)/device/linaro/hikey/installer/$(TARGET_OUT_DIR)
 
 # CLANG is provided as a RO env var by soong
@@ -24,29 +28,21 @@ FIP_BIN ?= $(DIST_DIR_PRODUCT)/fip.bin
 # from optee_os/mk/aosp_optee.mk when building.
 OPTEE_OS_DIR=optee/optee_os
 
-clean:
+# rebuild fip.bin whenever optee_os is modified
+# build_uefi.sh will always clean before build by default
+$(FIP_BIN): $(sort $(shell find -L $(OPTEE_OS_DIR)))
 	@# Should we really rm $(PRODUCT_OUT)/optee built by optee_os/aosp_optee.mk and NOT by this file?
 	rm -rf $(DIST_DIR_PRODUCT) $(PRODUCT_OUT)/optee
-	cd $(LLOADER_DIR)/../ && \
-		CLEAN=2 $(LLOADER_DIR)/build_uefi.sh
-
-# rebuild fip.bin whenever optee_os is modified
-# clean before build
-$(FIP_BIN): clean $(sort $(shell find -L $(OPTEE_OS_DIR)))
-	cd $(LLOADER_DIR)/../ && \
+	cd $(OPTEE_DIR) && \
 		CLANG=CLANG_5_0 CLANG_PATH=$(CLANG_PATH) $(LLOADER_DIR)/build_uefi.sh && \
 		cp fip.bin $(DIST_DIR_PRODUCT)/ && \
 		cp fip.bin $(INSTALLER_DIR)/ && \
-		cp l-loader.bin $(DIST_DIR_PRODUCT)/ && \
 		cp l-loader.bin $(INSTALLER_DIR)/ && \
-		cp ptable-*.img $(DIST_DIR_PRODUCT)/ && \
 		cp ptable-*.img $(INSTALLER_DIR)/
 ifneq ($(filter hikey960%, $(TARGET_OUT_DIR)),)
-	cd $(LLOADER_DIR) && \
-		cp recovery.bin $(DIST_DIR_PRODUCT)/ && \
-		cp recovery.bin $(INSTALLER_DIR)/ && \
-		cp $(TOOLS_DIR)/hikey_idt $(INSTALLER_DIR)/ && \
-		cp $(TOOLS_DIR)/hisi*.img $(INSTALLER_DIR)/
+	cp $(LLOADER_DIR)/recovery.bin $(INSTALLER_DIR)/
+	cp $(TOOLS_DIR)/hikey_idt $(INSTALLER_DIR)/
+	cp $(TOOLS_DIR)/hisi*.img $(INSTALLER_DIR)/
 endif
 
 droidcore: $(FIP_BIN)
