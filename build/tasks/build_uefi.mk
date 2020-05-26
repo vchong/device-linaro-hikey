92c4b7f9245a19e204850565e0b31eb0a3bf6c0b
Victor Chong <victor.chong@linaro.org>
2020-05-26 10:06:06 +0100

    works
    Change-Id: I4b79a2957aaf807c34c7dd7f457e889900c9bd75

--- build/tasks/build_uefi.mk
+++ build/tasks/build_uefi.mk
@@ -2,15 +2,24 @@ ifneq ($(filter hikey%, $(TARGET_DEVICE)),)
 
 ifeq ($(TARGET_BUILD_UEFI), true)
 
+# setting value for PREBUILT_MAKE to specify which value to use
+PREBUILT_MAKE ?= prebuilts/build-tools/linux-x86/bin/make
+ifneq (,$(wildcard $(PREBUILT_MAKE)))
+HOST_MAKE := $(PREBUILT_MAKE)
+ifneq (,$(filter -j%, $(MAKE)))
+HOST_MAKE += $(filter -j%, $(MAKE))
+endif
+else
+HOST_MAKE := $(MAKE)
+endif
+
 TOP_ROOT_ABS := $(realpath $(TOP))
 
 OPTEE_DIR=$(TOP_ROOT_ABS)/optee
 LLOADER_DIR=$(TOP_ROOT_ABS)/optee/l-loader
 TOOLS_DIR=$(TOP_ROOT_ABS)/optee/tools-images-hikey960
 
-# https://sx.ix5.org/info/post/android-q-compiling-kernels-in-tree
-# make target MUST beging with `out/..` and NOT abs path!
-DIST_DIR_PRODUCT?=$(OUT_DIR)/dist
+BOOTLOADER_DIR ?= device/linaro/hikey/bootloader
 
 INSTALLER_DIR?=$(TOP_ROOT_ABS)/device/linaro/hikey/installer/$(TARGET_OUT_DIR)
 
@@ -21,7 +30,11 @@ CLANG_PATH=$(TOP_ROOT_ABS)/$(LLVM_PREBUILTS_PATH)/
 # Please use DEBUG=1 (default) in build_uefi.sh due to
 # https://bugs.96boards.org/show_bug.cgi?id=806
 
-FIP_BIN ?= $(DIST_DIR_PRODUCT)/fip.bin
+# https://sx.ix5.org/info/post/android-q-compiling-kernels-in-tree
+# make target MUST beging with `out/..` and NOT abs path!
+FIP_BIN ?= $(OUT_DIR)/dist/fip.bin
+
+HOST_PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # Do NOT prepend $(TOP_ROOT_ABS) here! Else get error
 # make: *** /home/first.last/aosp//home/first.last/aosp/optee/optee_os: No such file or directory.  Stop.
@@ -32,18 +45,16 @@ OPTEE_OS_DIR=optee/optee_os
 # build_uefi.sh will always clean before build by default
 $(FIP_BIN): $(sort $(shell find -L $(OPTEE_OS_DIR)))
 	@# Should we really rm $(PRODUCT_OUT)/optee built by optee_os/aosp_optee.mk and NOT by this file?
-	rm -rf $(DIST_DIR_PRODUCT) $(PRODUCT_OUT)/optee
-	cd $(OPTEE_DIR) && \
-		CLANG=CLANG_5_0 CLANG_PATH=$(CLANG_PATH) $(LLOADER_DIR)/build_uefi.sh && \
-		cp fip.bin $(DIST_DIR_PRODUCT)/ && \
-		cp fip.bin $(INSTALLER_DIR)/ && \
-		cp l-loader.bin $(INSTALLER_DIR)/ && \
-		cp ptable-*.img $(INSTALLER_DIR)/
-ifneq ($(filter hikey960%, $(TARGET_OUT_DIR)),)
-	cp $(LLOADER_DIR)/recovery.bin $(INSTALLER_DIR)/
-	cp $(TOOLS_DIR)/hikey_idt $(INSTALLER_DIR)/
-	cp $(TOOLS_DIR)/hisi*.img $(INSTALLER_DIR)/
-endif
+	echo "## TOP = $(TOP)"
+	echo "## TOP_ROOT_ABS = $(TOP_ROOT_ABS)"
+	echo "## TARGET_OUT_DIR = $(TARGET_OUT_DIR)"
+	echo "## TARGET_OUT = $(TARGET_OUT)"
+	echo "## TARGET_OUT_VENDOR = $(TARGET_OUT_VENDOR)"
+	mkdir -p $(OUT_DIR)
+	rm -rf $@ $(PRODUCT_OUT)/optee
+	@# Does NOT build without '-j1'!
+	PATH=$(HOST_PATH):$$PATH $(HOST_MAKE) -j1 -C $(BOOTLOADER_DIR) TOP_ROOT_ABS=$(TOP_ROOT_ABS) CLANG_PATH=$(CLANG_PATH)
+	cp $(LLOADER_DIR)/fip.bin $@
 
 droidcore: $(FIP_BIN)
 endif #TARGET_BUILD_UEFI
